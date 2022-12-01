57370e5df250d9f70c36e69e91fa31a50f3d5951
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-12-01 08:18:45 +0530

    Added:Retrive_Data_Joined_in_particular_date_range

--- src/com/bridgelabz/JdbcEmployeePayrollService.java
+++ src/com/bridgelabz/JdbcEmployeePayrollService.java
@@ -6,7 +6,7 @@ public class JdbcEmployeePayrollService {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/employee_payrolldata"; // table details
         String username = "root"; // MySQL credentials
-        String password = "Namdev@120";
+        String password = "Aditya@120";
         try {
             Class.forName(
                     "com.mysql.cj.jdbc.Driver"); // Driver name
@@ -14,44 +14,42 @@ public class JdbcEmployeePayrollService {
                     url, username, password);
             System.out.println(
                     "Connection Established successfully");
-            PreparedStatement st = con.prepareStatement("Select * from employee_details");
-            PreparedStatement st1 = con.prepareStatement("Update salary_details Set BasicPay=? where Salary_id=?");
-            st1.setInt(1,3000000);
-            st1.setInt(2,5);
-            boolean result = st1.execute();
+            PreparedStatement st = con.prepareStatement("Select * from salary_details");
+            PreparedStatement st2= con.prepareStatement("Select * from salary_details where StartDate Between '2018-09-21' AND '2018-09-24'");
+            boolean result = st2.execute();
             if (result) {
-                ResultSet rs = st1.getResultSet();
+                ResultSet rs = st2.getResultSet();
                 while (rs.next()) {
                     int id
-                            = rs.getInt("EmployeeID"); // Retrieve name from db
+                            = rs.getInt("salary_id"); // Retrieve name from db
                     System.out.println(id); // Print result on console
                     String name
-                            = rs.getString("EmployeeName"); // Retrieve name from db
+                            = rs.getString("BasicPay"); // Retrieve name from db
                     System.out.println(name); // Print result on console
                     String gender
-                            = rs.getString("Gender"); // Retrieve name from db
+                            = rs.getString("Deductions"); // Retrieve name from db
                     System.out.println(gender); // Print result on console
                     String city
-                            = rs.getString("City"); // Retrieve name from db
+                            = rs.getString("TaxablePay"); // Retrieve name from db
                     System.out.println(city); // Print result on console
                     String state
-                            = rs.getString("Address"); // Retrieve name from db
+                            = rs.getString("Tax"); // Retrieve name from db
                     System.out.println(state); // Print result on console
                     String PhoneNumber
-                            = rs.getString("PhoneNumber"); // Retrieve name from db
+                            = rs.getString("NetPAy"); // Retrieve name from db
                     System.out.println(PhoneNumber); // Print result on console
                     String country
-                            = rs.getString("Country"); // Retrieve name from db
+                            = rs.getString("StarDAte"); // Retrieve name from db
                     System.out.println(country); // Print result on console
                 }
-//            }else{
-//                int rowcount=st1.getMaxRows();
-//                System.out.println(rowcount);
-//            }
-            } else {
-                int rowCount = st1.getUpdateCount();
-                System.out.println(rowCount);
+            }else{
+                int rowcount=st2.getMaxRows();
+                System.out.println(rowcount);
             }
+//            } else {
+//                int rowCount = st.getUpdateCount();
+//                System.out.println(rowCount);
+//            }
 
             con.close(); // close connection
             System.out.println("Connection Closed....");
