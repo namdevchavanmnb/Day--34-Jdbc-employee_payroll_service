aa5796f6b8805fc21b88f82c42534e9e6c4baf3f
namdevchavanmnb <namdevchavanmnb@gmail.com>
2022-12-01 08:04:08 +0530

    git commit -m

--- src/com/bridgelabz/JdbcEmployeePayrollService.java
+++ src/com/bridgelabz/JdbcEmployeePayrollService.java
@@ -1,7 +1,27 @@
 package com.bridgelabz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcEmployeePayrollService {
     public static void main(String[] args) {
-        System.out.println("Welcome to the JDBC Program");
+        String url = "jdbc:mysql://localhost:3306/employee_payrolldata"; // table details
+        String username = "root"; // MySQL credentials
+        String password = "Namdev@120";
+        try {
+            Class.forName(
+                    "com.mysql.cj.jdbc.Driver"); // Driver name
+            Connection con = DriverManager.getConnection(
+                    url, username, password);
+            System.out.println(
+                    "Connection Established successfully");
+            con.close(); // close connection
+            System.out.println("Connection Closed....");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println(e);
+        }
     }
 }
+
+
